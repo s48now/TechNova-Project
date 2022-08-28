8d67d74452fd3665700f68bd2390f4a3ac8bc9d2
s48now <112220951+s48now@users.noreply.github.com>
2022-08-27 22:50:03 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import ProfileScreen from './screens/Profile';
-import SettingsScreen from './screens/Settings';
+import Search from './screens/SearchTab';
+import LearnMore from './screens/LearnMore';
+/*
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
-import SavedScreen from './screens/Saved';
-import ReferScreen from './screens/Refer';
-import DrawerItems from './constants/DrawerItems';
-import { StatusBar } from 'expo-status-bar';
+*/
+import AboutUs from './screens/AboutUs';
+import DrawerItems from './DrawerItems';
+//import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
